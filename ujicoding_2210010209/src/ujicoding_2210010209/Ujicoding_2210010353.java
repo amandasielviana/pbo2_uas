e866a34af17d43853f06bd8a820131a0cf9d0e85
amandasielviana <gusti.amanda@icloud.com>
2025-01-16 23:20:34 +0800

    Add files via upload

---
+++ ujicoding_2210010209/src/ujicoding_2210010209/Ujicoding_2210010353.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ujicoding_2210010209;
+import javax.swing.JOptionPane;
+import sys.crudtable;
+import gui.framenilai;
+/**
+ *
+ * @author gustiamanda
+ */
+public class Ujicoding_2210010353{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new framenilai().setVisible(true);
+    }
+    
+}
